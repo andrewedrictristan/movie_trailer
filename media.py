18d78fe9c4b81ac7679d125d12c8732bfbd59770
andrewedrictristan <andrew_tristan23@outlook.com>
2018-07-26 23:31:40 +0200

    first commit

---
+++ media.py
@@ -0,0 +1,15 @@
+import webbrowser
+
+class Movie():
+"""Class movie is consist of constructor and method"""
+
+    def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
+        """ Inits is the constructor where its parameter will get the value from module entertainment_center """
+        self.title = movie_title
+        self.storyline = movie_storyline
+        self.poster_image_url = poster_image
+        self.trailer_youtube_url = trailer_youtube
+
+    def show_trailer(self):
+        """ show_trailer_function can be called to open the function that open trailer in browser """
+        webbrowser.open(self.trailer_youtube_url)
