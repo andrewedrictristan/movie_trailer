56ceff6321f7136bcaa95c9cce0cabc89681372f
andrewedrictristan <andrew_tristan23@outlook.com>
2018-07-28 10:59:59 +0200

    last update

--- entertainment_center.py
+++ entertainment_center.py
@@ -3,38 +3,43 @@ import fresh_tomatoes
 
 # Creating the objects of movies
 
-rampage = media.Movie("Rampage",
-                      "A story about the Gorilla that grows like giant because of experiment",
-                      "http://t1.gstatic.com/images?q=tbn:ANd9GcQpTCKCvU_Fz0SwP_oyuSSKdf_unn88WWa5evQC4F3U7EfHyQVJ",
-                      "https://www.youtube.com/watch?v=coOKvrsmQiI")
+rampage = media.Movie(
+    "Rampage",
+    "A story about the Gorilla that grows like giant because of experiment",
+    "images/rampage.jpeg",
+    "https://www.youtube.com/watch?v=coOKvrsmQiI")
 
-jumanji = media.Movie("Jumanji",
-                     "4 kids in game world",
-                     "http://t2.gstatic.com/images?q=tbn:ANd9GcSGLiKxLm4FSbjNzysaI8xVoUEE27RDObZg9pSiP28nvSEwy7Mb",
-                     "hhttps://www.youtube.com/watch?v=2QKg5SZ_35I")
-
-
-batman = media.Movie("Batman: the dark knight rises",
-                     "A story about the hero in Gotham city",
-                     "http://t1.gstatic.com/images?q=tbn:ANd9GcQSGF8_VbDqKF0B_4IQ0NF87VMDSy7_MPKryawR-qLnSIPQwo5z",
-                     "https://www.youtube.com/watch?v=GokKUqLcvD8")
-
-madmax= media.Movie("Mad Max",
-                    "Story about the hero in wasteland",
-                    "http://t0.gstatic.com/images?q=tbn:ANd9GcQuK41mExh1Qv3kbXoxohWYGlcstOQ6zEnnNdSI2BGIKywQwgRI",
-                    "https://www.youtube.com/watch?v=hEJnMQG9ev8")
+jumanji = media.Movie(
+    "Jumanji",
+    "4 kids in game world",
+    "images/jumanji.jpeg",
+    "https://www.youtube.com/watch?v=2QKg5SZ_35I")
 
 
-ratatouille = media.Movie("Ratatouille",
-                          "Storyline",
-                          "http://www.gstatic.com/tv/thumb/movieposters/165058/p165058_p_v8_aj.jpg",
-                          "https://www.youtube.com/watch?v=c3sBBRxDAqk")
+batman = media.Movie(
+    "Batman: the dark knight rises",
+    "A story about the hero in Gotham city",
+    "images/batman.jpeg",
+    "https://www.youtube.com/watch?v=GokKUqLcvD8")
 
-hunger_games = media.Movie("Hunger Games",
-                          "Storyline",
-                          "http://t2.gstatic.com/images?q=tbn:ANd9GcS58mYVyiI3LTihImFjn6QBLU_mcHXZP3LaGoWN9u5bzuvW3lvC",
-                          "https://www.youtube.com/watch?v=mfmrPu43DF8"
-                        )
+madmax = media.Movie(
+    "Mad Max",
+    "Story about the hero in wasteland",
+    "images/madmax.jpeg",
+    "https://www.youtube.com/watch?v=hEJnMQG9ev8")
+
+
+ratatouille = media.Movie(
+    "Ratatouille",
+    "Storyline",
+    "images/rat.jpg",
+    "https://www.youtube.com/watch?v=c3sBBRxDAqk")
+
+hunger_games = media.Movie(
+    "Hunger Games",
+    "Storyline",
+    "images/hunger.jpeg",
+    "https://www.youtube.com/watch?v=mfmrPu43DF8")
 
 
 #Creating the list of movies from the created objects
--- media.py
+++ media.py
@@ -1,15 +1,17 @@
 import webbrowser
 
+
 class Movie():
     """Class movie is consist of constructor and method"""
-
-    def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
-        """ Inits is the constructor where its parameter will get the value from module entertainment_center """
+    def __init__(self, movie_title, storyline, poster_image, trailer_youtube):
+        """Inits is the constructor where its parameter will get the value
+        from module entertainment_center"""
         self.title = movie_title
-        self.storyline = movie_storyline
+        self.storyline = storyline
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
 
     def show_trailer(self):
-        """ show_trailer_function can be called to open the function that open trailer in browser """
+        """ show_trailer_function can be called to open
+        the function that open trailer in browser """
         webbrowser.open(self.trailer_youtube_url)
